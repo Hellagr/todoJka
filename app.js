57197c13239b66af112e6e107bbe3a838579e08b
Hellagr <98410790+Hellagr@users.noreply.github.com>
2022-11-27 22:29:33 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const Userpanel = require('./models/userpanel')
 
-// mongoose.connect('mongodb://localhost:27017/todojkaApp', { useNewUrlParser: true, useUnifiedTopology: true, family: 4 })
-//     .then(() => {
-//         console.log("MONGO CONNECTION OPEN!!!")
-//     })
-//     .catch(err => {
-//         console.log("OH NO MONGO CONNECTION ERROR!!!!")
-//         console.log(err)
-//     })
 
+mongoose.connect('mongodb://localhost:27017/todojka', { useNewUrlParser: true, useUnifiedTopology: true, family: 4 })
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+})
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "node_modules/bootstrap/dist/")));
+app.use(express.static(path.join(__dirname, "views")));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.render('home')
-})
+});
+
+
+
 
 app.listen(3000, () => {
     console.log("It's OK!")
