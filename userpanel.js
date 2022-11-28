4aef2e498db5947e0bc8171063293149ea510683
Hellagr <98410790+Hellagr@users.noreply.github.com>
2022-11-28 06:54:58 +0300

    Delete userpanel.js

--- userpanel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const taskSchema = new Schema({
-    title: String,
-    task: String
-})
-
-module.exports = mongoose.model('Userpanel', taskSchema);
