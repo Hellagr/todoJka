d53668f73e9bf957005a03f009641d36a639b2d6
Hellagr <stanlywtf@gmail.com>
2023-01-22 22:05:00 +0300

    Add dynamic id code

--- views/scripts.js
+++ views/scripts.js
@@ -4,14 +4,30 @@ function hideAddcard() {
     document.getElementById('addCard').style.display = 'inline';
 }
 
-
-
 const editBtn = document.querySelector('#editBtn');
+const cardValue = document.querySelector('#valueIncard');
+
+// Gave a dynamic id to card id
+const allElements = document.querySelectorAll('*');
+const idArr = [];
+let x = 0;
+for (let i = 0; i < allElements.length; ++i) {
+    let el = allElements[i];
+    if (el.id == 'valueIncard') {
+        idArr.push(x);
+        document.getElementById("valueIncard").id = x;
+        document.getElementById("editBtn").id = x;
+        x++;
+    }
+}
+
+
+
+
+
+
 editBtn.addEventListener('click', function () {
+    document.getElementById().style.display = "none";
+})
 
-    var card = document.getElementById('card'),
-        childDiv = card.getElementsByTagName('div')[0].style.display = "none";
-    // document.getElementsByTagName(childDiv).style.display = "none";
-    document.getElementById('editForm').style.display = "inline";
 
-});
