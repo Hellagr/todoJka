ae8da871589541cc0b6a4cf880697f88964744ed
Hellagr <stanlywtf@gmail.com>
2023-01-23 22:11:46 +0300

    :(

--- views/scripts.js
+++ views/scripts.js
@@ -4,12 +4,13 @@ function hideAddcard() {
     document.getElementById('addCard').style.display = 'inline';
 }
 
-const editBtn = document.querySelector('#editBtn');
+var editBtn = document.querySelector('#editBtn');
 const cardValue = document.querySelector('#valueIncard');
 
 // Gave a dynamic id to card id
 const allElements = document.querySelectorAll('*');
 const idArr = [];
+
 let x = 0;
 for (let i = 0; i < allElements.length; ++i) {
     let el = allElements[i];
@@ -21,13 +22,24 @@ for (let i = 0; i < allElements.length; ++i) {
     }
 }
 
-
-
+const allBtn = document.querySelectorAll('button');
+const idBtn = [];
+for (let i = 0; i < allBtn.length; ++i) {
+    let la = allBtn[i];
+    if (la.id == i) {
+        idBtn.push(i);
+    }
+}
+console.log(allBtn)
+console.log(idBtn)
 
 
 
 editBtn.addEventListener('click', function () {
-    document.getElementById().style.display = "none";
+    if (idArr.id == idBtn.id) {
+        console.log('hello matafaka')
+    }
 })
+
 
 
