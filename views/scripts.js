054c2d398de7233e931505754e6c536a4860877b
Hellagr <stanlywtf@gmail.com>
2023-01-25 20:31:37 +0300

    I got it! Finnaly! Edit Card function is worked!

--- views/scripts.js
+++ views/scripts.js
@@ -4,42 +4,71 @@ function hideAddcard() {
     document.getElementById('addCard').style.display = 'inline';
 }
 
-var editBtn = document.querySelector('#editBtn');
-const cardValue = document.querySelector('#valueIncard');
-
-// Gave a dynamic id to card id
+const valueIncard = document.querySelector('.valueIncard');
+// get a new array of object with a valueIncard class
 const allElements = document.querySelectorAll('*');
 const idArr = [];
-
-let x = 0;
 for (let i = 0; i < allElements.length; ++i) {
     let el = allElements[i];
-    if (el.id == 'valueIncard') {
-        idArr.push(x);
-        document.getElementById("valueIncard").id = x;
-        document.getElementById("editBtn").id = x;
-        x++;
+    if (el.className == 'valueIncard') {
+        idArr.push(allElements[i]);
+    }
+}
+// get a new array of object with an idEditform class
+const idEditform = [];
+for (let i = 0; i < allElements.length; ++i) {
+    let el = allElements[i];
+    if (el.className == 'editForm') {
+        idEditform.push(allElements[i]);
+    }
+}
+// compare id of elements and link it
+function reply_click(buttonId) {
+    for (let i = 0; i < idArr.length; ++i) {
+        let el = idArr[i];
+        let idEdform = idEditform[i];
+        if (el.id == buttonId) {
+            if (idEdform.id == buttonId) {
+                el.style.display = 'none';
+                idEdform.style.display = 'inline';
+            }
+        }
     }
 }
 
-const allBtn = document.querySelectorAll('button');
-const idBtn = [];
-for (let i = 0; i < allBtn.length; ++i) {
-    let la = allBtn[i];
-    if (la.id == i) {
-        idBtn.push(i);
-    }
-}
-console.log(allBtn)
-console.log(idBtn)
 
 
 
-editBtn.addEventListener('click', function () {
-    if (idArr.id == idBtn.id) {
-        console.log('hello matafaka')
-    }
-})
+
+// let x = 0;
+// for (let i = 0; i < allElements.length; ++i) {
+//     let el = allElements[i];
+//     if (el.id == 'valueIncard') {
+//         idArr.push(x);
+//         document.getElementById("valueIncard").id = x;
+//         document.getElementById("editBtn").id = x;
+//         x++;
+//     }
+// }
+
+// const allBtn = document.querySelectorAll('button');
+// const idBtn = [];
+// for (let i = 0; i < allBtn.length; ++i) {
+//     let la = allBtn[i];
+//     if (la.id == i) {
+//         idBtn.push(i);
+//     }
+// }
+// console.log(allBtn)
+// console.log(idBtn)
+
+
+
+// editBtn.addEventListener('click', function () {
+//     if (idArr.id == idBtn.id) {
+//         console.log('hello matafaka')
+//     }
+// })
 
 
 
