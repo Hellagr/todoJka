de9c0454fe4bf395153824ac565cc4f60f7c5333
Hellagr <stanlywtf@gmail.com>
2023-02-01 21:22:37 +0300

    add Error handler

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const path = require('path');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const Taskpanel = require('./models/taskpanel');
-const morgan = require('morgan')
-
-
+const morgan = require('morgan');
+const AppError = require('./AppError');
 
 mongoose.connect('mongodb://localhost:27017/todojka', { useNewUrlParser: true, useUnifiedTopology: true, family: 4 })
 
@@ -24,39 +23,47 @@ app.use(express.static(path.join(__dirname, "models/taskpanel")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-app.use(morgan('tiny'))
+function wrapAsync(fn) {
+    return function (req, res, next) {
+        fn(req, res, next).catch(e => next(e))
+    }
+}
 
-app.get("/", async (req, res) => {
+app.get("/", wrapAsync(async (req, res, next) => {
     const taskpanels = await Taskpanel.find({});
     res.render('home', { taskpanels })
-});
+}));
 
 app.get("/404", async (req, res) => {
     res.render('404')
 });
 
-app.post("/", async (req, res) => {
+app.post("/", wrapAsync(async (req, res) => {
     const taskpanel = new Taskpanel(req.body.taskpanel);
     await taskpanel.save();
     res.redirect(`/`);
-});
+}));
 
-app.put('/:id', async (req, res) => {
+app.put('/:id', wrapAsync(async (req, res) => {
     const { id } = req.params;
     const taskpanel = await Taskpanel.findByIdAndUpdate(id, { ...req.body.taskpanel });
     res.redirect('/');
-});
+}));
 
-app.delete('/:id', async (req, res) => {
+app.delete('/:id', wrapAsync(async (req, res) => {
     const { id } = req.params;
     await Taskpanel.findByIdAndDelete(id);
     res.redirect('/');
-});
+}));
 
 app.use((req, res) => {
     res.redirect('/404')
 });
 
+app.use((err, req, res, next) => {
+    const { status = 500, message = 'Something went wrong!' } = err;
+    res.status(status).send(message);
+})
 
 app.listen(3000, () => {
     console.log("It's OK!")
