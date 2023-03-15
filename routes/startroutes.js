76b740c0cc9af919ce9259213c87c6cf5916f305
Hellagr <stanlywtf@gmail.com>
2023-03-15 21:07:22 +0300

    fix restr. routes

--- routes/startroutes.js
+++ routes/startroutes.js
@@ -5,11 +5,11 @@ const Taskpanel = require('../models/taskpanel');
 const User = require('../models/user');
 const passport = require('passport');
 const { middlewareAuth } = require('../middlewareAuth');
+const { validateTaskpanel } = require('../middlewareAuth');
 const { route } = require('./Users');
 const { authorize } = require('passport');
-const { populate, db } = require('../models/taskpanel');
 const taskpanelController = require('../controllers/taskpanel');
-const { validateTaskpanel } = require('../middlewareAuth');
+
 
 router.route('/')
     .get(middlewareAuth, wrapAsync(taskpanelController.home))
@@ -19,8 +19,8 @@ router.route('/:id')
     .put(middlewareAuth, validateTaskpanel, wrapAsync(taskpanelController.changeTask))
     .delete(middlewareAuth, wrapAsync(taskpanelController.deleteTask));
 
-router.get('/completed', middlewareAuth(taskpanelController.completed));
+router.get('/completed', middlewareAuth, taskpanelController.completed);
 
-router.get('/deleted', middlewareAuth(taskpanelController.deleted));
+router.get('/deleted', middlewareAuth, taskpanelController.deleted);
 
 module.exports = router;
